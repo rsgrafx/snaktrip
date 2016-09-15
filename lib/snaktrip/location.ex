cc38f573f6dc7d33c9a03233a61cc01e125c3111
Orion Engleton <rsgrafx@gmail.com>
2016-09-14 20:51:25 -0700

    Import Snaptrip.Repo

--- lib/snaktrip/location.ex
+++ lib/snaktrip/location.ex
@@ -1,9 +1,10 @@
 defmodule Snaktrip.Location do
-
   @moduledoc """
     Represents the Location resource that builds up a Snaptrip
     %Snaktrip.Location{ address: "string", name: "string", latlong: [lat, long] }
   """
   defstruct id: nil, address: nil, name: nil, latlong: nil
+
+  use Snaktrip.RethinkDB.Repo
 
 end
