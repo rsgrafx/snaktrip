876df638d8b18763352eb0116cf65042e12f1b6d
Orion Engleton <rsgrafx@gmail.com>
2016-09-14 20:38:35 -0700

    Removed database in favor of Schema

--- lib/snaktrip/database.ex
+++
@@ -1,9 +0,0 @@
-defmodule Snaktrip.Database do
-  use RethinkDB.Connection
-
-  @moduledoc """
-    Queries can be run without providing a connection (it will use the name connection).
-    import RethinkDB.Query
-      table("locations") |> SnakTrip.Database.run
-  """
-end
