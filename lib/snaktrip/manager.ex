939571b7fa44e1df20e60e3b2cb175d2ac559117
Orion Engleton <rsgrafx@gmail.com>
2016-09-15 15:46:54 -0700

    Setup two new entry points to create Snaktrip server processes - adding those processes to the manager cache

--- lib/snaktrip/manager.ex
+++ lib/snaktrip/manager.ex
@@ -13,21 +13,51 @@ defmodule Snaktrip.Manager do
     GenServer.start_link(__MODULE__, %{}, name: :snak_manager)
   end
 
+  @doc """
+    Calls into the Cache
+      when your are sure of the <ID> of the snaktrip
+      # Snaktrip.Server Process
+      returns #PID< Snaktrip. process>
+  """
+  def fetch(snaktrip_uuid) do
+    GenServer.call(:snak_manager, {:server_process, snaktrip_uuid} )
+  end
 
   @doc """
-    Calls into the Cache - fetches or creates server process which
-    maintains the state of individual snaktrip.
+    Calls into the Cache - creates server process which
+      maintains the state of individual snaktrip.
+      # This is a entry point for app
+      # -> pass in the user-id- * API-TOKEN -
+      # Snaktrip.Server Process
+      returns #PID< Snaktrip. process> ( new record )
   """
-
-  def fetch(manager_pid, snaktrip_uuid) do
-    GenServer.call(manager_pid, {:server_process, snaktrip_uuid} )
+  def start_new(owner_id: owner_id) do
+    GenServer.call(:snak_manager, {:server_process, owner_id: owner_id})
   end
 
   def init(state) do
     {:ok, state}
   end
 
+  def handle_call({:server_process, owner_id: owner_id}, snak_servers) do
+    do_handle_call(snak_servers, owner_id: owner_id)
+  end
+
   def handle_call({:server_process, snaktrip_uuid}, _, snak_servers) do
+    do_handle_call(snak_servers, snaktrip_uuid)
+  end
+
+  def do_handle_call(snak_servers, owner_id: owner_id) do
+    {:ok, server} = Snaktrip.Server.start_link(owner_id: owner_id)
+
+    %{id: snaktrip_uuid} = Snaktrip.Server.get(server)
+
+    snak_servers = Map.put(snak_servers, snaktrip_uuid, server)
+    
+    {:reply, server, snak_servers}
+  end
+
+  def do_handle_call(snak_servers, snaktrip_uuid) do
     case Map.fetch( snak_servers, snaktrip_uuid ) do
       {:ok, server_id} ->
         {:reply, server_id, snak_servers}
