476240b7b91410075c7ddde77d4f3cb828ff8902
Orion Engleton <rsgrafx@gmail.com>
2016-09-15 20:43:29 -0700

    removed incorrect directive

--- lib/snaktrip_app.ex
+++ lib/snaktrip_app.ex
@@ -1,16 +1,38 @@
 defmodule SnaktripApp do
-
   use Application
 
+  def start_cowboy do
+
+    dispatch = :cowboy_router.compile([ {:_, routes} ] )
+
+    {:ok, _} = :cowboy.start_http(:snaktrip_app, 100, [{:port, 8090}],
+
+    [{:env, [{:dispatch, dispatch}]}])
+  end
+
+
   def start _, _ do
+
+    start_cowboy
+
     import Supervisor.Spec, warn: false
 
     children = [
       worker(Snaktrip.Connection, [[db: "snaktrip_app"]]),
-      worker(Snaktrip.Manager, [])
+      worker(Snaktrip.Manager, []),
+      worker(Snaktrip.Web.Router, [], function: :run)
+
     ]
     opts = [strategy: :one_for_one, name: SnaktripApp.Supervisor]
 
     Supervisor.start_link(children, opts)
   end
+
+  def routes do
+    [
+      {"/socket", Snaktrip.Web.Socket, []},
+      {:_, Plug.Adapters.Cowboy.Handler, {Snaktrip.Web.Router, []}}
+    ]
+  end
+
 end
