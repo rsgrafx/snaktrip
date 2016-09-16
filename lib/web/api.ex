6149931f5c91f01572ca62c9421607ea82b4da4f
Orion Engleton <rsgrafx@gmail.com>
2016-09-16 16:43:08 -0700

    Endpoint sends back the API-Token

--- lib/web/api.ex
+++ lib/web/api.ex
@@ -1,15 +1,44 @@
 defmodule Snaktrip.Web.Api do
+
   use Plug.Router
 
   plug :match
   plug :dispatch
+  plug :fetch_query_params
 
-  get "/snaktrip/:uuid" do
+  plug Plug.Parsers, parsers: [:urlencoded, :json],
+                   pass:  ["text/*"],
+                   json_decoder: Poison
+
+  get "/snaktrips" do
+    %{data: data} = Snaktrip.all
+    resp = %{data: data}
     conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, %{data: [
-        %{id: 1, locations: %{}}, %{id: 2, locations: %{}}
-      ]} |> Poison.encode!)
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, resp |> Poison.encode!)
+  end
+
+  post "/user/account" do
+    resp =
+      conn
+      |> read_body
+      |> json
+
+      %{"email" => email } = resp
+
+    resp =
+      Snaktrip.User.Manager.fetch(email)
+      |> Snaktrip.User.Server.get
+      |> Map.from_struct
+      |> IO.inspect
+
+    conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, resp |> Poison.encode!)
+  end
+
+  def json({:ok, body, conn}) do
+    Poison.decode! body
   end
 
 end
