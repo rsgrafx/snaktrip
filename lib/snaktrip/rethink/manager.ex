ee26be55126b9360c7c07bab77f2e81552fbca02
Orion Engleton <rsgrafx@gmail.com>
2016-09-16 01:36:07 -0700

    Basic Genserver to maintain list of tables from rethink

---
+++ lib/snaktrip/rethink/manager.ex
@@ -0,0 +1,41 @@
+defmodule Snaktrip.RethinkDB.Manager do
+  use GenServer
+  @doc """
+    Simple genserver to keep list of
+    All tables in Rethink system so as to
+    not have to query all the time *
+      *
+  """
+  def start_link(rethink_connection) do
+    GenServer.start_link(__MODULE__, rethink_connection, name: :rethinkdb_manager)
+  end
+
+  def all_tables do
+    GenServer.call(:rethinkdb_manager, :tables)
+  end
+
+  def add_table(table_name) do
+    GenServer.cast(:rethinkg_manager, {:add_table, table_name} )
+  end
+
+  # Callbacks do
+
+  def init(conn) do
+    send(self(), :fetch_tables)
+    {:ok, conn}
+  end
+
+  def handle_cast({:add_table, table_name}, state) do
+    {:noreply, [table_name|state]}
+  end
+
+  def handle_call(:tables, _, state) do
+    {:reply, state, state}
+  end
+
+  def handle_info(:fetch_tables, conn) do
+    %{data: list} = RethinkDB.Query.table_list |> conn.run
+    {:noreply, list}
+  end
+
+end
