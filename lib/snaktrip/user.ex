133fd041384d9f7f8d2ac151d6e6f87181e7abb1
Orion Engleton <rsgrafx@gmail.com>
2016-09-18 00:07:48 -0700

    modified defstruct add current_location

--- lib/snaktrip/user.ex
+++ lib/snaktrip/user.ex
@@ -1,6 +1,6 @@
 defmodule Snaktrip.User do
-  
-  defstruct id: nil, email: nil, snaktrips: []
+
+  defstruct id: nil, email: nil, snaktrips: [], current_location: nil
 
   use Snaktrip.RethinkDB.Repo
 end
