1054ac39d3c11e9a942361c7fe6dbba864e3fe5b
Orion Engleton <rsgrafx@gmail.com>
2016-09-19 15:20:49 -0700

    Snaktrip.Server processes are now under gproc registration - the registry maintains the list so that these processes can be more easily found throughout the system - With this inplace - and the Snaktrip.Manager itself being supervised - if the process dies I can fetch all processes that have been scoped to the naming {:snak_servers, "<val>"} and rebuild the list - as added measure check if that individual process is alive and restart it and reassign it on a process by process level in the handle_call

--- lib/snaktrip/manager.ex
+++ lib/snaktrip/manager.ex
@@ -23,6 +23,10 @@ defmodule Snaktrip.Manager do
     GenServer.call(:snak_manager, {:server_process, snaktrip_uuid} )
   end
 
+  def all do
+    GenServer.call(:snak_manager, :all)
+  end
+
   @doc """
     Calls into the Cache - creates server process which
       maintains the state of individual snaktrip.
@@ -36,7 +40,26 @@ defmodule Snaktrip.Manager do
   end
 
   def init(state) do
+    send self(), :rebuild
     {:ok, state}
+  end
+
+  def handle_info(:rebuild, _) do
+    :gproc.lookup_pids({:n, :l, {:snak_servers, :"_"}})
+    |> case do
+      [] ->
+        {:noreply, %{} }
+      pids when is_list(pids) ->
+        {:noreply, Enum.reduce(pids, %{},  &build_key_map/2)}
+    end
+  end
+
+  defp build_key_map(pid, acc) do
+    Map.put_new(acc, Snaktrip.Server.get(pid).id, pid)
+  end
+
+  def handle_call(:all, _, state) do
+    {:reply, state, state}
   end
 
   def handle_call({:server_process, owner_id: owner_id}, snak_servers) do
@@ -49,21 +72,34 @@ defmodule Snaktrip.Manager do
 
   def do_handle_call(snak_servers, owner_id: owner_id) do
     {:ok, server} = Snaktrip.Server.start_link(owner_id: owner_id)
-
     %{id: snaktrip_uuid} = Snaktrip.Server.get(server)
-
     snak_servers = Map.put(snak_servers, snaktrip_uuid, server)
-    
     {:reply, server, snak_servers}
   end
-
+  #
+  #  :gproc.lookup_pids {:n, :l, {:snak_servers, "applejson" }} :: [#PID<0.425.0>]
+  #  :gproc.lookup_pids({:n, :l, {:snak_servers, :"_"}}) :: [#PID<0.425.0>, #PID<0.445.0>...]
+  #
   def do_handle_call(snak_servers, snaktrip_uuid) do
     case Map.fetch( snak_servers, snaktrip_uuid ) do
       {:ok, server_id} ->
-        {:reply, server_id, snak_servers}
+        if Process.alive? server_id do
+          {:reply, server_id, snak_servers}
+        else
+          :gproc.where({:n, :l, {:snak_servers, snaktrip_uuid}})
+          |> case do
+            found_pid when is_pid(found_pid) ->
+              snak_servers = Map.put(snak_servers, snaktrip_uuid, found_pid)
+              {:reply, found_pid, snak_servers}
+            :undefined ->
+              {:ok, new_pid} = Snaktrip.Supervisor.start_snaktrip(snaktrip_uuid)
+              snak_servers = Map.put(snak_servers, snaktrip_uuid, new_pid)
+              {:reply, new_pid, snak_servers}
+          end
+        end
       :error ->
         # Create new SnakTrip.Server
-        {:ok, server_id} = Snaktrip.Server.start_link(snaktrip_uuid)
+        {:ok, server_id} = Snaktrip.Supervisor.start_snaktrip(snaktrip_uuid)
         # Add to list
         snak_servers = Map.put(snak_servers, snaktrip_uuid, server_id)
         # Return
